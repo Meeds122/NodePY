fb3a99acbe2cd8c821dbf0ef31753b98a8a66742
Tristan Henning <tristan@customcrypto.com>
2019-05-26 15:03:51 -0700

    Initial commit

---
+++ client/request.js
@@ -0,0 +1,11 @@
+function request(call){
+    request_object = new XMLHttpRequest();
+    api_url = "http://127.0.0.1:81/" + call;
+    request_object.open("GET", api_url)
+    request_object.send();
+    request_object.onreadystatechange=function(){
+        if(this.readyState==4 && this.status==200){
+            return request_object.responseText
+        }
+    }
+}
