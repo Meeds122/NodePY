2bb15fecd083004e1fa5afe6034008cc37ad1ca9
Tristan Henning <tristan@customcrypto.com>
2019-05-28 20:33:31 -0700

    Fixed client side asyncronous request failure

--- client/request.js
+++ client/request.js
@@ -1,4 +1,4 @@
-function request(call){
+function x_request(call){
     request_object = new XMLHttpRequest();
     api_url = "http://127.0.0.1:31337/" + call;
     request_object.open("GET", api_url)
@@ -9,3 +9,22 @@ function request(call){
         }
     }
 }
+
+function request(call, cb){
+    var myurl = "http://127.0.0.1:31337/" + call;
+    var xmlhttp;
+    if (window.XMLHttpRequest){
+        xmlhttp=new XMLHttpRequest();}
+    else{
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");}
+ 
+     xmlhttp.onreadystatechange=function(){
+         if (xmlhttp.readyState==4 && xmlhttp.status==200){
+             if( typeof cb === 'function' )
+                 cb(xmlhttp.responseText);
+         }
+     }
+ 
+    xmlhttp.open("GET",myurl,true);
+    xmlhttp.send();
+ }
