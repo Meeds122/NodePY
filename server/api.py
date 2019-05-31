af31ff05c38bad93842ce057145d5c19ab89c074
Tristan Henning <tristan@customcrypto.com>
2019-05-30 23:30:22 -0700

    Modified notes

--- server/api.py
+++ server/api.py
@@ -11,12 +11,12 @@ Content-Length: CON_LEN
 Content-Type: text/html
 Connection: Closed
 
-R_DATA
-    """
+R_DATA"""
     current_time = str(time.strftime("%a, %d %b %Y %H:%M:%S PST"))
     basic_response = basic_response.replace("TIME", current_time)
     basic_response = basic_response.replace("R_DATA", str(data))
     data_len = len(basic_response) - 7 # 7 is the length of the CON_LEN identifier that will be replaced
+    #data_len = data_len + 2 # troubleshooting bug where JS complains about size mismatch. I checked with Wireshark and my initial calculations are correct. 
     data_len = data_len + len(str(data_len))
     basic_response = basic_response.replace("CON_LEN", str(data_len))
     return basic_response
