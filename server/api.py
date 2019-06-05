79d1ade5e5e5e6f462adb48419b71526ef00f6bf
Tristan Henning <tristan@customcrypto.com>
2019-06-04 23:35:43 -0700

    Continued development

--- server/api.py
+++ server/api.py
@@ -6,12 +6,38 @@ import sys
 sys.path.insert(0, './calls') # Include py files in calls/ 
 import serverTime
 
+# Globals
+API_CALLS = {} # a dictionary that contains a map of api calls to api functions.
+
+# Testing
+API_CALLS['serverTime'] = serverTime.main
+
 """
 call(request):
     Takes request, calls apropriate function, returns data from function. 
 """
 def call(request):
-    return "API called + " + str(request) + str(serverTime.main(request))
+    global API_CALLS # dictionary of known API calls
+
+    # Sanity checks
+    if len(request) <= 1:
+        return "BAD REQUEST"
+
+    # Format request into [Request without preceding slash, arg1, argc]
+    if request.find('&') != -1:
+        api_call = request.split("&")
+    else:
+        api_call = [request,]
+    api_call[0] = api_call[0][1:]
+
+    # Check if call function is already in dictionary of calls. 
+    try:
+        f = API_CALLS[api_call[0]]
+    except KeyError:
+        # Do see if API py file exists. If exists, add, otherwise return BAD REQUEST
+        pass
+    
+    return str(f(api_call[1:]))
 
 
 """
--- server/calls/serverTime.py
+++ server/calls/serverTime.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 
-def main(when):
+def main(in_list):
     import time
+
+    # Values are passed in as a list of arguments i.e. arg1 = [arg1, arg2, etc. ]
+    # In this case, we only care about the first value
+
+    when = in_list[0]
+
+    # Testing
+    print(in_list)
+    print(when)
 
     if when.lower == 'now':
         return "now: " + str(time.asctime( time.localtime(time.time()) ))
